bd3269183aad4e51a6f1c28d1733283c38b76896
neeleshp26 <38248207+neeleshp26@users.noreply.github.com>
2022-12-16 14:22:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.header('🍞 Dinner menu')
 streamlit.text(' 🐔 Murg Khalipha')
 streamlit.text('Daru tochan')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
