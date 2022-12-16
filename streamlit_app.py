0cf76635f8d116f389ab8a4856f6ac7876101ded
neeleshp26 <38248207+neeleshp26@users.noreply.github.com>
2022-12-16 13:23:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
 streamlit.title('My friends are happy')
 streamlit.header('🍞 Dinner menu')
 streamlit.text(' 🐔 Murg Khalipha')
 streamlit.text('Daru tochan')
+my_fruit_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+streamlit.dataframe(my_fruit_list)
