e0dbfb458f988db6ee5d325828a2c591b9cbc109
neeleshp26 <38248207+neeleshp26@users.noreply.github.com>
2022-12-16 13:19:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('🥑My friends are happy')
-streamlit.header('🍞Dinner menu')
-streamlit.text('Murg Khalipha')
+streamlit.title('My friends are happy')
+streamlit.header('🍞 Dinner menu')
+streamlit.text(' 🐔 Murg Khalipha')
 streamlit.text('Daru tochan')
